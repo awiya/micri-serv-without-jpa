0797133ebd2665fb63f5613b66e6454fae499c5b
AYOUB <a.idihia@gmail.com>
2023-05-05 13:43:31 +0200

    Création du service de configuration comme un repo de configuration des microservices

--- config-server/src/test/java/io/awiya/configserver/ConfigServerApplicationTests.java
+++ config-server/src/test/java/io/awiya/configserver/ConfigServerApplicationTests.java
@@ -1,10 +1,10 @@
-package io.awiya.servicedepartements;
+package io.awiya.configserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ServiceDepartementsApplicationTests {
+class ConfigServerApplicationTests {
 
 	@Test
 	void contextLoads() {
---
+++ department-server/src/test/java/io/awiya/servicedepartements/DepartmentServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.awiya.servicedepartements;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartmentServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
