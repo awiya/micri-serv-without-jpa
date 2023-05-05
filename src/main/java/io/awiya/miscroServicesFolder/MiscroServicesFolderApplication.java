d9aa82eeb4d107115084c3064914f0bbaf89ecac
AYOUB <a.idihia@gmail.com>
2023-05-05 12:54:38 +0200

    Premier Commit

---
+++ src/main/java/io/awiya/miscroServicesFolder/MiscroServicesFolderApplication.java
@@ -0,0 +1,13 @@
+package io.awiya.miscroServicesFolder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiscroServicesFolderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiscroServicesFolderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/awiya/miscroServicesFolder/MiscroServicesFolderApplicationTests.java
@@ -0,0 +1,13 @@
+package io.awiya.miscroServicesFolder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiscroServicesFolderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
